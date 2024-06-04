afb7eadb75163987512d310a18007e27552bd359
Bpd369qrt <juancescudero17@gmail.com>
2024-06-03 23:02:26 -0300

    Subo de nuevo por error anterior

---
+++ src/main/java/gc/_4/pr2/grupo3/entity/Usuario.java
@@ -0,0 +1,46 @@
+package gc._4.pr2.grupo3.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Usuario {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+
+	private Long id;
+	private String nombre;
+	private String gmail;
+	private String contraseña;
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getGmail() {
+		return gmail;
+	}
+	public void setGmail(String gmail) {
+		this.gmail = gmail;
+	}
+	public String getContraseña() {
+		return contraseña;
+	}
+	public void setContraseña(String contraseña) {
+		this.contraseña = contraseña;
+	}
+	
+	
+	
+}
