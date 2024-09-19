7a3c31bd450d72519131b4a852f557e6ade132fc
Nicolas Benitez <nikobenitez2902@gmail.com>
2024-09-19 01:07:04 -0300

    Persistencia de eventos

--- src/main/java/gc/_4/pr2/grupo3/controller/EventosController.java
+++ src/main/java/gc/_4/pr2/grupo3/controller/EventosController.java
@@ -1,30 +1,53 @@
 package gc._4.pr2.grupo3.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import gc._4.pr2.grupo3.entity.Eventos;
+import gc._4.pr2.grupo3.service.IEventosService;
 
-@RestController
+@RestController 
 
 public class EventosController {
-	
-@GetMapping ("/eventos")
-	
-	public Eventos infoDeEvento() {
-		
-		Eventos eventos = new Eventos();
-		
-		eventos.setId(null);
-		eventos.setIdUsuario(null);
-		eventos.setNombreEvento(null);
-		eventos.setFechaHoraEvento(null);
-		eventos.setUbicacionEvento(null);
-		eventos.setDescripcionEvento(null);
-		
-		return eventos;
-		
-		
-	}
 
+    @Autowired
+
+    private IEventosService service;
+
+    @GetMapping("/evento")
+    public List<Eventos> motrarTodosLosEvento(){
+        return service.mostrarTodo();
+
+    }
+
+    @GetMapping("/evento/{id}")
+    public Eventos mostrarEventosPorId(@PathVariable("id") Long id){
+        return service.mostrarPorId(id);
+
+    }
+
+    @PostMapping("/evento")
+    public Eventos crearNuevoEvento(@RequestBody Eventos eventosDesdeElServicio){
+        return service.guardar(eventosDesdeElServicio);
+
+    }
+
+    @PutMapping("/evento")
+    public Eventos actualizarNuevoEvento(@RequestBody Eventos eventosDesdeElServicio){
+        return service.guardar(eventosDesdeElServicio);
+
+    }
+
+    @DeleteMapping("/grupo{id}")
+    public void borrarGrupo(@PathVariable("id") Long id){
+        service.eliminarPorId(id);
+    }
 }
