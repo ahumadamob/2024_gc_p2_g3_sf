3efe826624306056a0e662531fd8a6644938542d
TiagoLanza <lanzatiago42@gmail.com>
2024-11-07 23:35:18 -0300

    Se sube la persistencia avanzada de Contacto

--- src/main/java/gc/_4/pr2/grupo3/controller/ContactoController.java
+++ src/main/java/gc/_4/pr2/grupo3/controller/ContactoController.java
@@ -1,6 +1,8 @@
 package gc._4.pr2.grupo3.controller;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.NoSuchElementException;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -11,6 +13,7 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import gc._4.pr2.grupo3.dto.RespuestaDto;
 import gc._4.pr2.grupo3.entity.Contacto;
 import gc._4.pr2.grupo3.service.IContactoService;
 
@@ -18,38 +21,79 @@ import gc._4.pr2.grupo3.service.IContactoService;
 
 public class ContactoController {
 
-    @Autowired
+	@Autowired
+	private IContactoService service;
+	
+	
+	@GetMapping("/Contacto")
+	public RespuestaDto<List<Contacto>> buscarTodos(){
+		List<Contacto> listaContacto;
+		listaContacto = new ArrayList();
+		listaContacto = service.mostrarTodo();
 
-    private IContactoService service;
+		RespuestaDto<List<Contacto>> dto;
+		dto = new RespuestaDto<List<Contacto>>();		
 
-    @GetMapping("/contacto")
-    public List<Contacto> motrarTodosLosContacto(){
-        return service.mostrarTodo();
+		if(listaContacto.isEmpty()) {
+			dto.setEstado(false);
+			List<String> mensajes = new ArrayList();
+			mensajes.add("No se encontraron Contactos");
+			dto.setMensaje(mensajes);
+			dto.setData(null);
+		}else {
+			List<String> mensajes = new ArrayList();
+			mensajes.add("Se encontraron los siguientes Contactos");
+			mensajes.add("Todo salio bien");
+			dto.setEstado(true);
+			dto.setMensaje(mensajes);
+			dto.setData(listaContacto);
+		}		
+		return dto;
 
-    }
+	}
+	@GetMapping("/Contacto/{id}")
+	public RespuestaDto<Contacto> buscarPorId(@PathVariable("id") Long id){
+		if(service.existe(id)) {
+			Contacto contacto = new Contacto();
+			contacto = service.mostrarPorId(id);
+			RespuestaDto<Contacto> dto;
+			dto = new RespuestaDto<Contacto>(true, "OK", contacto);
+			return dto;
+		}else {			
+			return new RespuestaDto<Contacto>(false, "No existen contactos con el id ", null);
+		}
 
-    @GetMapping("/contacto/{id}")
-    public Contacto mostrarContactosPorId(@PathVariable("id") Long id){
-        return service.mostrarPorId(id);
 
-    }
+	}
+	
 
-    @PostMapping("/contacto")
-    public Contacto crearNuevoContacto(@RequestBody Contacto contactosDesdeElServicio){
-        return service.guardar(contactosDesdeElServicio);
+	@PostMapping("/api/contacto")
+    public RespuestaDto<Contacto> crearNuevoNota(@RequestBody Contacto contacto) {
+        if (service.existe(contacto.getId())) {
+            return new RespuestaDto<>(false, "Error: El ID ya existe", null);
+        }
+        Contacto contactoGuardado = service.guardar(contacto); 
+        return new RespuestaDto<>(true, "Contacto creado exitosamente", contactoGuardado);
+        		}
 
-    }
+	@PutMapping("/Contacto")
+	public RespuestaDto<Contacto> actualizar(@RequestBody Contacto contactoDesdeElPostman){
+		if(service.existe(contactoDesdeElPostman.getId())) {
+			return new RespuestaDto<Contacto>(true, "Contacto actualizado con exito", service.guardar(contactoDesdeElPostman));
+		}else {
+			return new RespuestaDto<Contacto>(false, "No se encontró el ID " + contactoDesdeElPostman.getId().toString(), null);
+		}		
+	}
+	@DeleteMapping("/Contacto/{ideliminar}")
+	public RespuestaDto<?> eliminar(@PathVariable("ideliminar") Long id){
+		if(service.existe(id)) {
+			service.eliminarPorId(id);
+			return new RespuestaDto<>(true, "Elemento eliminado el ID: " + id.toString(), null);
+		}else {
+			return new RespuestaDto<>(false, "No se encontró el ID " + id.toString(), null);
+		}
 
-    @PutMapping("/contacto")
-    public Contacto actualizarNuevoContacto(@RequestBody Contacto contactosDesdeElServicio){
-        return service.guardar(contactosDesdeElServicio);
+	}
 
-    }
-
-    @DeleteMapping("/contacto{id}")
-    public void borrarContacto(@PathVariable("id") Long id){
-        service.eliminarPorId(id);
-    }
 }
-
 
--- src/main/java/gc/_4/pr2/grupo3/service/IContactoService.java
+++ src/main/java/gc/_4/pr2/grupo3/service/IContactoService.java
@@ -5,9 +5,10 @@ import java.util.List;
 import gc._4.pr2.grupo3.entity.Contacto;
 
 public interface IContactoService {
-
 	List<Contacto>mostrarTodo();
 	Contacto mostrarPorId(Long id);
 	Contacto guardar(Contacto contactos);
 	void eliminarPorId(Long id);
+	boolean existe(Long id);
+	
 }
